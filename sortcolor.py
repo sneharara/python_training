2df108884e3f08ecbfb3367b6e9932a0bcad84c1
sneharara <sneharara971@gmail.com>
2026-02-11 19:09:44 +0530

    Add files via upload

---
+++ sortcolor.py
@@ -0,0 +1,17 @@
+nums = [2,0,2,1,1,0]
+low = 0
+mid = 0
+high= len(nums) -1
+while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+                
+            elif nums[mid] == 1:
+                mid += 1
+                
+            else:  # nums[mid] == 2
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
+print(nums)
